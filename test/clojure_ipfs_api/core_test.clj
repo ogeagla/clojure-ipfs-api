b2a18003d5229866eeea8769085de4f5ce1eb474
Octavian Geagla <ogeagla@gmail.com>
2017-11-30 13:34:07 -0800

    [genesis] initial commit

---
+++ test/clojure_ipfs_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-ipfs-api.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-ipfs-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
